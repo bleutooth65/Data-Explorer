40b6d985b6d576de5da17cb7ccc3ac04462fe351
bleutooth65 <51833105+bleutooth65@users.noreply.github.com>
2021-04-28 21:37:52 -0400

    Steady state for back end with pandas

--- widgets/widgets.py
+++ widgets/widgets.py
@@ -19,204 +19,142 @@ from wx.lib.mixins.listctrl import ListCtrlAutoWidthMixin
 def NotImplemented(self): 
       wx.MessageBox("Currently not implemented", "Not Implemented" ,wx.OK | wx.ICON_WARNING)
 
-def ListParser():
-	"""
-	A parser for list columns, where each list is composed of pairs of values.
-	"""
+# class VirtualListCtrl(wx.ListCtrl, ListCtrlAutoWidthMixin):
+# 	"""
+# 	A generic virtual list.
+# 	"""
 
-	value = Regex(r'[-+]?[0-9]+(?:\.[0-9]*)?(?:e[-+]?[0-9]+)?', IGNORECASE)
-	value.setParseAction(lambda toks: float(toks[0]))
+# 	max_value_len = 20 # Characters.
 
-	item = Suppress('(') + value + Suppress(',') + value + Suppress(')')
-	item.setParseAction(tuple)
+# 	@staticmethod
+# 	def find_type(value):
+# 		"""
+# 		Determine the type of a column based on a single value.
 
-	lst = Suppress('[') + delimitedList(item) + Suppress(']')
-	lst.setParseAction(list)
+# 		The type is one of: scalar, list, string.
+# 		"""
 
-	def parse(s):
-		try:
-			return lst.parseString(s).asList()
-		except ParseBaseException as e:
-			raise ValueError(e)
+# 		try:
+# 			float(value)
+# 		except ValueError:
+# 			pass
+# 		else:
+# 			return 'scalar'
 
-	return parse
+# 		try:
+# 			ListParser()(value)
+# 		except ValueError:
+# 			pass
+# 		else:
+# 			return 'list'
 
+# 		return 'string'
 
-class VirtualListCtrl(wx.ListCtrl, ListCtrlAutoWidthMixin):
-	"""
-	A generic virtual list.
-	"""
+# 	def __init__(self, parent, *args, **kwargs):
+# 		wx.ListCtrl.__init__(self, parent,
+# 				style=wx.LC_REPORT|wx.LC_VIRTUAL|wx.LC_HRULES|wx.LC_VRULES,
+# 				*args, **kwargs)
 
-	max_value_len = 20 # Characters.
+# 		ListCtrlAutoWidthMixin.__init__(self)
 
-	@staticmethod
-	def find_type(value):
-		"""
-		Determine the type of a column based on a single value.
+# 		self.reset()
 
-		The type is one of: scalar, list, string.
-		"""
+# 	def reset(self):
+# 		self.headings = []
+# 		self.data = np.array([])
+# 		self.filtered_data = None
+# 		self.display_data = np.array([])
 
-		try:
-			float(value)
-		except ValueError:
-			pass
-		else:
-			return 'scalar'
+# 		self.types = []
 
-		try:
-			ListParser()(value)
-		except ValueError:
-			pass
-		else:
-			return 'list'
+# 	def refresh_with_values(self, data):
+# 		self.ItemCount = len(data)
 
-		return 'string'
+# 		if self.ItemCount > 0:
+# 			self.display_data = np.zeros(data.shape, dtype=f'|S{self.max_value_len}')
 
-	def __init__(self, parent, *args, **kwargs):
-		wx.ListCtrl.__init__(self, parent,
-				style=wx.LC_REPORT|wx.LC_VIRTUAL|wx.LC_HRULES|wx.LC_VRULES,
-				*args, **kwargs)
+# 			for i, _ in enumerate(self.headings):
+# 				# Truncate for display.
+# 				self.display_data[:,i] = [str(x)[:self.max_value_len] for x in data[:,i]]
 
-		ListCtrlAutoWidthMixin.__init__(self)
+# 		self.Refresh()
 
-		self.reset()
+# 	def apply_filter(self, f, afresh=False):
+# 		"""
+# 		Set the data to be the old data, along with the application of a filter.
 
-	def reset(self):
-		self.headings = []
-		self.data = np.array([])
-		self.filtered_data = None
-		self.display_data = np.array([])
+# 		f is a function of two parameters: the index of the row and the row itself.
+# 		f must return True if the row is to be kept and False otherwise.
 
-		self.types = []
+# 		If afresh is True, all old filtered data is discarded.
+# 		Otherwise, a new filter can be quickly applied.
+# 		"""
 
-	def refresh_with_values(self, data):
-		self.ItemCount = len(data)
+# 		if afresh:
+# 			self.filtered_data = None
 
-		if self.ItemCount > 0:
-			self.display_data = np.zeros(data.shape, dtype=f'|S{self.max_value_len}')
+# 		if self.filtered_data is not None:
+# 			original_set = self.filtered_data
+# 		else:
+# 			original_set = self.data
 
-			for i, _ in enumerate(self.headings):
-				# Truncate for display.
-				self.display_data[:,i] = [str(x)[:self.max_value_len] for x in data[:,i]]
+# 		self.filtered_data = compress([f(i, x) for i, x in enumerate(original_set)], original_set, axis=0)
 
-		self.Refresh()
+# 		self.refresh_with_values(self.filtered_data)
 
-	def apply_filter(self, f, afresh=False):
-		"""
-		Set the data to be the old data, along with the application of a filter.
+# 	def GetValue(self, types=None):
+# 		# Get all types by default.
+# 		if types is None:
+# 			types = set(self.types)
+# 		else:
+# 			types = set(types)
 
-		f is a function of two parameters: the index of the row and the row itself.
-		f must return True if the row is to be kept and False otherwise.
+# 		# Find column indices of the correct type.
+# 		idxs = [i for i, t in enumerate(self.types) if t in types]
 
-		If afresh is True, all old filtered data is discarded.
-		Otherwise, a new filter can be quickly applied.
-		"""
+# 		if self.filtered_data is not None:
+# 			data = self.filtered_data
+# 		else:
+# 			data = self.data
 
-		if afresh:
-			self.filtered_data = None
+# 		return ([self.headings[i] for i in idxs], data[:,idxs], [self.types[i] for i in idxs])
 
-		if self.filtered_data is not None:
-			original_set = self.filtered_data
-		else:
-			original_set = self.data
+# 	def SetValue(self, headings, data):
+# 		"""
+# 		headings: A list of strings.
+# 		data: A 2D NumPy array.
+# 		"""
 
-		self.filtered_data = compress([f(i, x) for i, x in enumerate(original_set)], original_set, axis=0)
+# 		self.ClearAll()
+# 		self.reset()
 
-		self.refresh_with_values(self.filtered_data)
+# 		self.headings = headings
+# 		self.data = data
 
-	def GetValue(self, types=None):
-		# Get all types by default.
-		if types is None:
-			types = set(self.types)
-		else:
-			types = set(types)
+# 		self.refresh_with_values(self.data)
 
-		# Find column indices of the correct type.
-		idxs = [i for i, t in enumerate(self.types) if t in types]
+# 		if self.ItemCount > 0:
+# 			width, height = self.GetSize()
+# 			# Give some room for the scrollbar.
+# 			col_width = (width - 50) / len(self.headings)
 
-		if self.filtered_data is not None:
-			data = self.filtered_data
-		else:
-			data = self.data
+# 			for i, heading in enumerate(self.headings):
+# 				self.InsertColumn(i, heading, width=col_width)
 
-		return ([self.headings[i] for i in idxs], data[:,idxs], [self.types[i] for i in idxs])
+# 				type = self.find_type(data[0,i])
+# 				self.types.append(type)
 
-	def SetValue(self, headings, data):
-		"""
-		headings: A list of strings.
-		data: A 2D NumPy array.
-		"""
+# 	def OnGetItemText(self, item, col):
+# 		"""
+# 		Return cell value for LC_VIRTUAL.
+# 		"""
 
-		self.ClearAll()
-		self.reset()
-
-		self.headings = headings
-		self.data = data
-
-		self.refresh_with_values(self.data)
-
-		if self.ItemCount > 0:
-			width, height = self.GetSize()
-			# Give some room for the scrollbar.
-			col_width = (width - 50) / len(self.headings)
-
-			for i, heading in enumerate(self.headings):
-				self.InsertColumn(i, heading, width=col_width)
-
-				type = self.find_type(data[0,i])
-				self.types.append(type)
-
-	def OnGetItemText(self, item, col):
-		"""
-		Return cell value for LC_VIRTUAL.
-		"""
-
-		return self.display_data[item,col]
-
-class DataTable(wx.grid.GridTableBase):
-    def __init__(self, data=None):
-        wx.grid.GridTableBase.__init__(self)
-        self.headerRows = 1
-        if data is None:
-            data = pd.DataFrame()
-        self.data = data
-
-    def GetNumberRows(self):
-        return len(self.data)
-
-    def GetNumberCols(self):
-        return len(self.data.columns) + 1
-
-    def GetValue(self, row, col):
-        if col == 0:
-            return self.data.index[row]
-        return self.data.iloc[row, col - 1]
-
-    def SetValue(self, row, col, value):
-        self.data.iloc[row, col - 1] = value
-
-    def GetColLabelValue(self, col):
-        if col == 0:
-            if self.data.index.name is None:
-                return 'Index'
-            else:
-                return self.data.index.name
-        return str(self.data.columns[col - 1])
-
-    def GetTypeName(self, row, col):
-        return wx.grid.GRID_VALUE_STRING
-
-    def GetAttr(self, row, col, prop):
-        attr = wx.grid.GridCellAttr()
-        if row % 2 == 1:
-            attr.SetBackgroundColour('#CCE6FF')
-        return attr
+# 		return self.display_data[item,col]
 
 class Table_Viewer(wx.ListCtrl):
 	def __init__(self, parent):
 		wx.ListCtrl.__init__(self, parent,id=wx.ID_ANY, pos=wx.DefaultPosition,
-				size=wx.DefaultSize, style=wx.LC_REPORT|wx.LC_VRULES|wx.LC_VIRTUAL)
+				size=wx.DefaultSize, style=wx.LC_REPORT|wx.LC_VIRTUAL|wx.LC_VRULES|wx.LC_HRULES)
 		self.df=pd.DataFrame()
 #-----------------------------         
 	def set_value(self,df):
@@ -225,59 +163,14 @@ class Table_Viewer(wx.ListCtrl):
 		for i, col in enumerate(df):
 			self.InsertColumn(i,col)
 			self.SetColumnWidth(i, wx.LIST_AUTOSIZE_USEHEADER)
+		self.SetBackgroundColour(wx.Colour('#e6f3ff'))
+		self.EnableAlternateRowColours(enable=True)
 		self.SetItemCount(len(self.df))
 		self.Update()
 #----------------------------  
 	def OnGetItemText(self,item, col):
 			value = self.df.iloc[item, col]
 			return str(value)
-
-# class TabularDisplayPanel(wx.Panel):
-# 	"""
-# 	A panel to display arbitrary tabular data.
-# 	"""
-
-# 	def __init__(self, parent, *args, **kwargs):
-# 		wx.Panel.__init__(self, parent, *args, **kwargs)
-
-# 		# Panel.
-# 		# panel_box = wx.BoxSizer(wx.VERTICAL)
-
-# 		## Table.
-# 		self.table = DataTable()
-# 		# panel_box.Add(self.table, proportion=1, flag=wx.EXPAND)
-
-# 		# self.SetSizer(panel_box)
-
-# 	def __len__(self):
-# 		return self.table.ItemCount
-
-# 	# TODO: has headers does not function as intended, never will reach code to give header names
-# 	def from_csv_data(self, has_header, values):
-# 		"""
-# 		Import the given CSV data into the table.
-
-# 		If has_header is True, the first row is treated specially.
-# 		"""
-
-# 		if has_header:
-# 			headers, rows = values[0], np.array(values[1:])
-# 		else:
-# 			headers, rows = [''] * len(values[0]), np.array(values)
-
-# 		# Ensure that all columns have a header.
-# 		for i, header in enumerate(headers):
-# 			if not header:
-# 				headers[i] = f'Column {i + 1}'
-
-# 		self.SetValue(headers, rows)
-
-# 	def GetValue(self, *args, **kwargs):
-# 		return self.table.GetValue(*args, **kwargs)
-
-# 	def SetValue(self, headings, values):
-# 		self.table.SetValue(headings, values)
-
 
 class TabularDisplayFrame(wx.Frame):
 	"""
@@ -296,21 +189,21 @@ class TabularDisplayFrame(wx.Frame):
 		self.sizer.Add(self.add_table(data), 1, wx.EXPAND)
 		self.SetSizer(self.sizer)
 		self.sizer.SetSizeHints(self)
+		self.SetSize((800, 600))
 
 	def add_table(self, data):
 		table = Table_Viewer(self)
 		table.set_value(data)
+		self.SetSize((800, 600))
 		return table
 
 	def remove_table(self):
 		self.sizer.Clear(delete_windows=True)
 		self.sizer.SetSizeHints(self)
+		self.SetSize((800, 600))
 
 	def exit(self, event):
 		self.Destroy()
-
-	def kill(self):
-		pass
 
 class Plot(wx.Panel):
 	def __init__(self, parent, id=-1, dpi=None, **kwargs):
